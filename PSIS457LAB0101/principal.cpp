5d7f3f342e7a0c7a9c95f5fb6b3ac6f02355a75b
corso <corso@DESKTOP-NVV0FM4>
2022-04-02 23:48:48 -0400

    Agregar archivos de proyecto.

---
+++ PSIS457LAB0101/principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	std::cout << "Nombre Completo : Burlecot Matias Corso Flores" << std::endl;
+	cout << "Edad : 20" << endl;
+	cout << "Carrera : Ingenieria de Sistemas" << endl;
+	cout << "Altura : 1.89 metros" << endl;
+	cout << "Peso : 68 kg" << endl;
+	cout << "Cumpleaños : 15 de marzo" << endl;
+	cout << "Color favorito : Morado" << endl;
+	return 0;
+}
